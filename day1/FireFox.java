5444fe076e9b07f2996afb77e4274609a039dc91
DhivyaParthi <140045978+DhivyaParthi@users.noreply.github.com>
2023-07-23 14:48:07 +0530

    Add files via upload
    Week1Day1Assignments

---
+++ day1/FireFox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 100.2f;
+		String name = "firefox";
+		boolean visible = true;
+		int year = 1998;
+		char logo = 'm';
+		System.out.println("The version is" + version);
+		System.out.println("Name of the Browser is" + name);
+		System.out.println("Is browser available ? :" + visible);
+		System.out.println("Year of establishment" + year);
+		System.out.println("The logo of Browser" + logo);		
+		
+
+	}
+
+}
